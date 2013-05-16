efccb84598f3f40ad0f128974ad0dd5ada5f24d7
Jim Lawruk <jimlawruk@yahoo.com>
2013-05-15 23:11:18 -0400

    Clean-up code of the Home page.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,27 +14,10 @@ namespace lawrukmvc.Controllers
     [HandleError]
     public class HomeController : Controller
     {
-        public ActionResult Index() {
-                        
-            var buttons = new List<HomeButton>()
-            {
-                new HomeButton("About","/about", "http://farm8.staticflickr.com/7273/7503083928_a2974e2861_m.jpg", "About Me"),
-                new HomeButton("Metro", "/metro", "/content/i/metro.jpg", "Minutes until the train arrives for Metro stops that I care about"),
-                //new HomeButton("Resume", "http://careers.stackoverflow.com/lawruk", "http://www.savingthedream.org/_ui/images/persona-millenial.jpg", "Stackoverflow Resume"),
-                new HomeButton("Blog", "/blog", "http://farm7.static.flickr.com/6084/6142564278_5573968475_m.jpg", "Boring Blog Posts"),
-                new HomeButton("News", "/news", "http://farm4.staticflickr.com/3456/3239806077_373a9b78a1.jpg", "News"),
-                new HomeButton("Weather","/weather", "/content/i/sky.jpg", "Weather for where we typically are"),
-                
-                //new HomeButton("Rome", "/rome", "http://www.savingthedream.org/_ui/images/persona-millenial.jpg", "Rome site from 2002"),
-                new HomeButton("Race Results", "/races/results", "http://farm1.staticflickr.com/246/3263516895_4a921506ec_m.jpg", "Race results")
-
-            };
-
-            if (lawrukmvc.Helpers.Mobile.ShowMobileSite()) {
-              buttons.Add(new HomeButton("Videos", "/videos","http://farm3.staticflickr.com/2461/3711078629_c9307e6ef4_m.jpg", "Videos"));
-            }
+        public ActionResult Index() 
+        {            
             var homeViewModel = new HomeViewModel();
-            homeViewModel.HomeButtons = buttons;
+            homeViewModel.HomeButtons = GetHomeButtonsList();
             if (Helpers.Mobile.ShowMobileSite())
             {
                 return View("Index.Mobile", homeViewModel);
@@ -43,6 +26,25 @@ namespace lawrukmvc.Controllers
             {
                 return View(homeViewModel);
             }
+        }
+
+        private List<HomeButton> GetHomeButtonsList()
+        {
+            var buttons = new List<HomeButton>()
+            {
+                new HomeButton("About","/about", "http://farm8.staticflickr.com/7273/7503083928_a2974e2861_m.jpg", "About Me"),
+                new HomeButton("Metro", "/metro", "/content/i/metro.jpg", "Minutes until the train arrives for Metro stops that I care about"),               
+                new HomeButton("Blog", "/blog", "http://farm7.static.flickr.com/6084/6142564278_5573968475_m.jpg", "Boring Blog Posts"),
+                new HomeButton("News", "/news", "http://farm4.staticflickr.com/3456/3239806077_373a9b78a1.jpg", "News"),
+                new HomeButton("Weather","/weather", "/content/i/sky.jpg", "Weather for where we typically are"),                
+                new HomeButton("Race Results", "/races/results", "http://farm1.staticflickr.com/246/3263516895_4a921506ec_m.jpg", "Race results")
+            };
+
+            if (lawrukmvc.Helpers.Mobile.ShowMobileSite())
+            {
+                buttons.Add(new HomeButton("Videos", "/videos", "http://farm3.staticflickr.com/2461/3711078629_c9307e6ef4_m.jpg", "Videos"));
+            }
+            return buttons;
         }
              
         public ActionResult About() {return View(); }
@@ -59,51 +61,22 @@ namespace lawrukmvc.Controllers
                 var token = Request.Form["token"];
                 var rpx = new Helpers.Rpx("a00d4647b32434096f453a4a03358b12f8235eb1", "https://rpxnow.com");
                 var authInfo = rpx.AuthInfo(token);
-                var doc = XDocument.Load(new XmlNodeReader(authInfo));               
+                var doc = XDocument.Load(new XmlNodeReader(authInfo));
                 Session["displayName"] = doc.Root.Descendants("displayName").First().Value;
                 Session["identifier"] = doc.Root.Descendants("identifier").First().Value;
                 Session["verifiedEmail"] = doc.Root.Descendants("verifiedEmail").First().Value;
                 Session["preferredUsername"] = doc.Root.Descendants("preferredUsername").First().Value;
                 Session["rpxXml"] = authInfo.OuterXml;
             }
-            catch (Exception ex)
-            {
-                
-            }
-            return View();
-            //return Redirect("/");
+            catch { }
+            return View();           
         }
 
         public ActionResult Logout() {
             Session["displayName"] = null;
             Session["identifier"] = null;
             return Redirect("/");
-        }
-
-        public ActionResult RunBuild()
-        {
-            ViewData["Title"] = "Run Build";
-            string data = "";
-            try
-            {
-                System.Diagnostics.Process p = new System.Diagnostics.Process();
-                p.StartInfo.UseShellExecute = true;
-                p.StartInfo.FileName = "C:\\inetpub\\wwwroot\\svnupdate.bat";
-                p.StartInfo.WorkingDirectory = "C:\\inetpub\\wwwroot\\";
-                p.StartInfo.RedirectStandardOutput = false;
-
-                //p.Start();
-                //p.WaitForExit() ;
-                //data = "Well, we thik it built.";
-                data = "<a href=\"http://www.lawruk.com/runbuild\">click</a> 0" + Request.ServerVariables["X_FORWARDED_FOR"] + " 1" + Request.ServerVariables["HTTP_X_FORWARDED_FOR"] + " 2" + HttpContext.Request.Headers["X-Forwarded-For"] + " 3:" + Request.Headers["HTTP_X_FORWARDED_FOR"];
-            }
-            catch (Exception ex)
-            {
-                data = ex.Message;
-            }
-            ViewData["Data"] = data;
-            return View("TitleData");
-        }
+        }      
 
     }
 }
