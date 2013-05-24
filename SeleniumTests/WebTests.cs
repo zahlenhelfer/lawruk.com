d9b8567c10209539a3ccd4699a9dc20f178ce672
Jim Lawruk <jimlawruk@yahoo.com>
2013-05-24 11:41:34 -0400

    NuGet, SeleniumTests project, upgrade to MVC 4

---
+++ SeleniumTests/WebTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTests
+{
+    [TestClass]
+    public class WebTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var driver = new FirefoxDriver();
+            var verificationErrors = new StringBuilder();
+            driver.Navigate().GoToUrl("http://www.lawruk.com");
+            Assert.AreEqual("", verificationErrors.ToString());
+            driver.Quit();
+
+        }
+    }
+}
